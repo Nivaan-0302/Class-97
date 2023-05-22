9e9f2ca6631cc2c983ac49f7213fdda8fe846bae
Nivaan-0302 <95119261+Nivaan-0302@users.noreply.github.com>
2023-05-22 14:40:56 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,56 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyA4T0iQTyFxdRFMUwz83KaKWdZ5b8M7bOA",
+      authDomain: "kwitter-social-media-29ead.firebaseapp.com",
+      databaseURL: "https://kwitter-social-media-29ead-default-rtdb.firebaseio.com",
+      projectId: "kwitter-social-media-29ead",
+      storageBucket: "kwitter-social-media-29ead.appspot.com",
+      messagingSenderId: "791154280547",
+      appId: "1:791154280547:web:0b22ba5ba40e320f1f9056",
+      measurementId: "G-09TFF71CYN"
+    };
+    
+    // Initialize Firebase
+     firebase.initializeApp(firebaseConfig);
+
+     user_name = localStorage.getItem("user_name");
+
+
+     document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+   
+function addRoom() 
+{
+      room_name = document.getElementById("room_name").value;
+      
+      firebase.database().ref("/").child(room_name).update({
+            purpose : "adding room name"
+      });
+
+      localStorage.setItem("room_name", room_name);
+
+      window.location = "kwitter_page.html";
+}
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+      document.getElementById("output").innerHTML += row;
+      //End code
+      });});}
+getData();
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
+
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = "index.html";
+      
+}
+
+
